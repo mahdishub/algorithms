ec72fca0303ef1a02361b81db577b5e9135cefd4
Mahdi Ahmed <mahdiahmed1416@gmail.com>
2018-09-26 15:19:38 +0600

    Update Coin Change.cpp

--- Coin Change.cpp
+++ Coin Change.cpp
@@ -2,26 +2,34 @@
 
 using namespace std;
 
+#define inf 0x3f3f3f3f
+
 int len;
 int arr[100];
+int dp[1005][1005];
+
 
 int cc ( int index, int toMake ) {
-    int ans1,ans2;
+    int ans1 = inf,ans2 = inf;
+    if ( dp[index][toMake] != -1 ) return dp[index][toMake];
     if ( index == len ) {
         if ( toMake == 0 ) return 0;
-        else return INT_MAX;
+        else return inf;
     }
     ans1 =  cc(index+1,toMake);
     if ( toMake-arr[index] >= 0 )
         ans2 = cc( index,toMake-arr[index] ) + 1;
-    return min(ans1,ans2);
+    return dp[index][toMake] =  min(ans1,ans2);
 }
 
+
 int main () {
-   int toMake;
-   cin >> len >> toMake;
-   for ( int i= 0 ; i < len; i++ ) cin >> arr[i];
-   cout << cc(0,toMake);
+    memset(dp,-1,sizeof(dp));
+    int toMake;
+    cin >> len >> toMake;
+    for ( int i= 0 ; i < len; i++ ) cin >> arr[i];
+    cout << cc(0,toMake);
 
 
+    return 0;
 }
